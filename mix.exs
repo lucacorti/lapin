ea2eec564d99b78db5b816f77b603c9939e58ea6
Luca Corti <luca.corti@wind.it>
2017-10-13 16:57:45 +0200

    Release 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lapin.Mixfile do
   def project do
     [
       app: :lapin,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.5",
       description: "Elixir RabbitMQ Client",
       package: package(),
