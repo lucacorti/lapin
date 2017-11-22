c9e3e6f411fcc6f96362853ecb1f055b635b4fc3
Luca Corti <luca@fantacast.it>
2017-11-22 20:34:27 +0100

    Explicit references to Lapin.Connection

--- test/lapin_test.exs
+++ test/lapin_test.exs
@@ -2,8 +2,6 @@ defmodule LapinTest do
   use ExUnit.Case
   doctest Lapin
 
-  alias Lapin.Connection
-
   setup_all do
     exchange = "test_exchange"
     queue = "test_queue"
@@ -54,19 +52,19 @@ defmodule LapinTest do
   end
 
   test "hello_world_producer_can_publish", ctx do
-    {:ok, producer} = Connection.start_link(ctx.producer)
+    {:ok, producer} = Lapin.Connection.start_link(ctx.producer)
     :ok = Lapin.Connection.publish(producer, ctx.exchange, "", ctx.message)
     :ok = Lapin.Connection.close(producer)
   end
 
   test "hello_world_consumer_cant_publish", ctx do
-    {:ok, consumer} = Connection.start_link(ctx.consumer)
+    {:ok, consumer} = Lapin.Connection.start_link(ctx.consumer)
     {:error, _} = Lapin.Connection.publish(consumer, ctx.exchange, "", ctx.message)
     :ok = Lapin.Connection.close(consumer)
   end
 
   test "hello_world_passive_cant_publish", ctx do
-    {:ok, passive} = Connection.start_link(ctx.passive)
+    {:ok, passive} = Lapin.Connection.start_link(ctx.passive)
     {:error, _} = Lapin.Connection.publish(passive, ctx.exchange, "", ctx.message)
     :ok = Lapin.Connection.close(passive)
   end
