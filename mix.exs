ed432d089b5aa6ef80dd05b277ffb05edb681d40
Luca Corti <luca@fantacast.it>
2018-02-04 17:10:03 +0100

    Update ex_doc, bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lapin.Mixfile do
   def project do
     [
       app: :lapin,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.5",
       description: "Elixir RabbitMQ Client",
       source_url: "https://github.com/lucacorti/lapin",
